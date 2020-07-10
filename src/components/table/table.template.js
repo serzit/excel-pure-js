234014cf5d05654e8b0bb629fe86ccfcf9d120d4
serzit <serzit@yandex.ru>
2020-07-10 17:04:38 +0300

    finish base table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,10 +3,17 @@ const CODES = {
   Z: 90
 }
 
-function toCell(_, colIndex) {
-  return `
-  	<div class="cell" contenteditable data-col="${colIndex}"></div>
+function toCell(row) {
+  return function(_, colIndex) {
+    return `
+  	<div class="cell" 
+  	  contenteditable 
+  	  data-col="${colIndex}"
+  	  data-type="cell"
+  	  data-id="${row}:${colIndex}"
+  	></div>
 	`
+  }
 }
 
 function toColumn(col, index) {
@@ -48,12 +55,12 @@ export function createTable(rowsCount = 15) {
 
   rows.push(createRow(null, cols))
 
-  for (let i = 0; i < rowsCount; i++) {
+  for (let row = 0; row < rowsCount; row++) {
   	const cells = new Array(colsCount)
         .fill('')
-        .map(toCell)
+        .map(toCell(row))
         .join('')
-    rows.push(createRow(i + 1, cells))
+    rows.push(createRow(row + 1, cells))
   }
 
   return rows.join('')
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,10 +4,25 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubcribers = []
+
+    this.prepare()
   }
+
+  prepare() {}
 
   toHTML() {
     return ''
+  }
+
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubcribers.push(unsub)
   }
 
   init() {
@@ -16,6 +31,7 @@ export class ExcelComponent extends DomListener {
 
   destroy() {
     this.removeDOMListeners()
+    this.unsubcribers.forEach(unsub => unsub())
   }
 }
 
