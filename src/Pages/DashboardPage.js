6c4e232f5db6261db541bd404ab488dbe96fbe5f
serzit <serzit@yandex.ru>
2020-07-14 19:48:59 +0300

    dashboard beginning preset

--- src/Pages/DashboardPage.js
+++ src/Pages/DashboardPage.js
@@ -1,41 +1,39 @@
-<div class="db">
-	<div class="db__header">
+import {Page} from '@core/Page';
+import {$} from '@core/dom';
+
+export class DashboardPage extends Page {
+  getRoot() {
+  	return $.create('div', 'db').html(`
+  <div class="db__header">
 		<h1>Excel Dashboard</h1>
 	</div>
-
 	<div class="db__new">
 		<div class="db__view">
 			<a class="db__create" href="#">
 				Новая <br/> таблица
-			</a>
+      </a>
 		</div>
 	</div>
-
 	<div class="db__table db__view">
-
 		<div class="db__list-header">
 			<span>Название</span>
 			<span>Дата открытия</span>
 		</div>
-
 		<ul class="db__list">
-
 			<li class="db__record">
 				<a href="#">Таблица номер 1</a>
 				<strong>05.07.2020</strong>
 			</li>
-
 			<li class="db__record">
 				<a href="#">Таблица номер 1</a>
 				<strong>05.07.2020</strong>
 			</li>
-
 			<li class="db__record">
 				<a href="#">Таблица номер 1</a>
 				<strong>05.07.2020</strong>
 			</li>
-
 		</ul>
-
 	</div>
-</div>
+  	`)
+  }
+}
---
+++ src/Pages/ExcelPage.js
@@ -0,0 +1,37 @@
+import {Page} from '@core/Page';
+import {createStore} from '@core/createStore';
+import {rootReducer} from '@/redux/rootReducer';
+import {initialState} from '@/redux/initialState';
+import {debounce, storage} from '@core/utils';
+import {Header} from '@/components/header/Header';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
+import {Excel} from '@/components/excel/Excel';
+
+export class ExcelPage extends Page {
+  getRoot() {
+    console.log(this.params)
+    const store = createStore(rootReducer, initialState)
+
+    const stateListener = debounce(state => {
+      console.log('App State:', state)
+      storage('excel-state', state)
+    }, 300)
+    store.subscribe(stateListener)
+
+    this.excel = new Excel( {
+      components: [Header, Toolbar, Formula, Table],
+      store
+    })
+
+    return this.excel.getRoot()
+  }
+
+  afterRender() {
+  	this.excel.init()
+  }
+  destroy() {
+    this.excel.destroy()
+  }
+}
