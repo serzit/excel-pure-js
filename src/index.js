3ee8913af48217e8b4725c2e9cc5c790ea6f881d
serzit <serzit@yandex.ru>
2020-07-16 16:01:45 +0300

    Finish routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './scss/index.scss'
 import {Router} from '@core/routes/Router';
 import {DashboardPage} from '@/Pages/DashboardPage';
 import {ExcelPage} from '@/Pages/ExcelPage';
+import './scss/index.scss'
 
 new Router('#app', {
   dashboard: DashboardPage,
